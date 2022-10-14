cde153bb75e17b635c72744fdc4229f7b463356d
unknown <f20201429@goa.bits-pilani.ac.in>
2022-10-14 22:45:10 +0530

    5th commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x2FA67Fb23bD663F6F5034A46b73B3e48C298c8a0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "";
+const METADATA_URL = "https://nft-collection-k79y17gxg-theyash27.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
